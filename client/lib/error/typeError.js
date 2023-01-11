ccacfbbe1abba3127af790d473cfde63f1419ae0
Majesty-jun <hkwns01@gmail.com>
2023-01-11 09:10:00 +0900

    [추가]: Manipulation 학습 & insert, error 유틸 함수 생성

---
+++ client/lib/error/typeError.js
@@ -0,0 +1,11 @@
+const typeError = (message) => {
+  throw new TypeError(message);
+}
+
+const refError = (message) => {
+  throw new ReferenceError(message);
+}
+
+const syntaxError = (message) => {
+  throw new SyntaxError(message);
+}
