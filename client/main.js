1ad187021ebc75f649164a0575015d460675b028
Majesty-jun <hkwns01@gmail.com>
2023-01-13 13:18:06 +0900

    [추가]: 덧셈 계산기 완성

--- client/main.js
+++ client/main.js
@@ -1,13 +1,39 @@
+const firstInput = getNode('#firstNumber');
+const secondInput = getNode('#secondNumber');
+const submit = getNode('#done');
+const result = getNode('.result')
 
 
-console.log(window);
+function getInputValue(node) {
+  if(typeof node === 'string') node = getNode(node);
+  if(node.tagName !== 'INPUT') refError('getInputValue 함수는 input Element만 허용합니다.');
+  return node.value;
+}
 
+const sum = (valueA, valueB) => valueA + valueB;
 
+function clearContents(node) {
+  if(typeof node === 'string') node = getNode(node);
+  node.textContent = '';
+}
 
+function renderCalculator() {
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput);
+  let total = sum(firstValue, secondValue);
+  clearContents(result);
+  insertLast(result, total);
+}
 
+function handler(e) {
+  e.preventDefault();
+  renderCalculator();
+}
 
+function inputHandler() {
+  renderCalculator();
+}
 
-
-
-
-
+submit.addEventListener('click', handler);
+firstInput.addEventListener('change', inputHandler);
+secondInput.addEventListener('change', inputHandler);
